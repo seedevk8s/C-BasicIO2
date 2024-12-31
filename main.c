6958165bf2d2aeecef8c9f649bf986f7cdd024f0
seedevk8s <seedevk8s@gmail.com>
2024-12-31 11:43:42 +0900

    입력값에 대한 예외처리 리펙토링

--- main.c
+++ main.c
@@ -3,15 +3,36 @@
 
 int main(void) {
 	double a;
-	printf("실수를 입력하세요: ");
 
-	if (scanf("%lf", &a) != 1) {
-		printf("입력 오류가 발생했습니다.\n");
-		// 입력 버퍼에 남은 잘못된 문자열을 비워줌 (MSVC 한정)
-		fflush(stdin);
-		return 1;
+	while (1) {
+		printf("실수를 입력하세요 (0을 입력하면 종료): ");
+
+		// 실수(double)를 입력받되, 정상적으로 한 개를 읽지 못하면 에러 처리
+		int result = scanf("%lf", &a);
+
+		if (result == 1) {
+			// 정상 입력: 0 체크 후 0이면 종료
+			if (a == 0) {
+				printf("프로그램을 종료합니다.\n");
+				break;
+			}
+			// 정상 입력한 경우 출력
+			printf("입력한 숫자는 %.2f입니다. \n", a);
+		}
+		else {
+			// 입력 오류: 잘못된 입력이 있을 경우 에러 메시지 출력 후 입력 버퍼 비우기
+			printf("입력 오류입니다. 다시 입력하세요.\n");
+
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF) {
+				// 아무것도 안하고 소진만 함
+			}
+
+			// 입력 버퍼 비우기 후 다시 입력 받기
+			continue;
+		}
 	}
-	printf("입력한 숫자는 %.2f입니다. \n", a);
-	system("pause");
-	return 0;
+
+		system("pause");
+		return 0;
 }
